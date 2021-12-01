1a8ae0bdccf2e74e2abb403b7c7540de3a929173
fkneeland <fkneeland@figure.com>
2021-12-01 09:36:43 -0700

    Solved first puzzel

---
+++ puzzel_1.py
@@ -0,0 +1,38 @@
+
+def part1():
+    f = open("./Puzzel_files/puzzel_1.txt")
+    pastLine = f.readline()
+    line = f.readline()
+    count = 0
+
+    while line is not None and line != "":
+        if int(pastLine) < int(line):
+            count = count + 1
+        pastLine = line
+        line = f.readline()
+
+    print("Count: ", count)
+
+def part2():
+    f = open("./Puzzel_files/puzzel_1.txt")
+    a = f.readline()
+    b = f.readline()
+    c = f.readline()
+    d = f.readline()
+    count = 0
+
+    while d is not None and d != "":
+        first = int(a) + int(b) + int(c)
+        second = int(b) + int(c) + int(d)
+        if second > first:
+            count += 1
+        a = b
+        b = c
+        c = d
+        d = f.readline()
+
+    print("Second part:")
+    print(count)
+
+part1()
+part2()
